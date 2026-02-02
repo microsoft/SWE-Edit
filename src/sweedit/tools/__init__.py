7fa74e018abf1b3d7eb36b990525177caf7b9fe0
Arist12 <ykzhang@cs.wisc.edu>
2026-02-02 15:21:12 -0600

    fix: remove legacy tools

--- src/sweedit/tools/__init__.py
+++ src/sweedit/tools/__init__.py
@@ -8,20 +8,16 @@ from sweedit.tools.execute_bash import ExecuteBash, ExecuteBashSWEBench
 from sweedit.tools.finish import Finish
 from sweedit.tools.llm_editor import LLMEditor
 from sweedit.tools.llm_file_editor import LLMFileEditor
-from sweedit.tools.set_todo_list import SetTodoList
 from sweedit.tools.str_replace_editor import StrReplaceEditor
 from sweedit.tools.str_replace_editor_v2 import StrReplaceEditorV2
-from sweedit.tools.think import Think
 
 __all__ = [
     "Tool",
     "ExecuteBash",
     "ExecuteBashSWEBench",
-    "SetTodoList",
     "LLMEditor",
     "StrReplaceEditor",
     "StrReplaceEditorV2",
-    "Think",
     "Finish",
     "LLMFileEditor",
 ]
